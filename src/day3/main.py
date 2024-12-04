ff5bb063d1c47d4fed2b89cfd03c2c02ceff7b56
caner bas <caner.bas@statsperform.com>
2024-12-04 18:03:01 +0100

    day3 part 2

--- src/day3/main.py
+++ src/day3/main.py
@@ -3,22 +3,33 @@ from typing import List
 
 regex = re.compile(r"mul\(\d{1,10},\d{1,8}\)")
 
-def parse_text(path: str) -> List[str]:
-    with open(path) as f:
-        corrupted_memory = f.read()
-    return re.findall(regex, corrupted_memory)
+
+def parse_text(text: str) -> List[str]:
+    return re.findall(regex, text)
+
 
 def calculate_result(functions: List[str]) -> int:
     result = 0
     for f in functions:
-        result += int(f.split(',',2)[0][4:]) * int(f.split(',',2)[1][:-1])
+        result += int(f.split(',', 2)[0][4:]) * int(f.split(',', 2)[1][:-1])
     return result
 
+
 def main() -> None:
-    functions = parse_text("input.txt")
-    print(calculate_result(functions))
+    with open('input.txt') as f:
+        corrupted_memory = f.read()
+    functions = parse_text(corrupted_memory)
+    print(f"part 1 {calculate_result(functions)}")
+    print(f"part 2 {calculate_result(parse_text(process_do_dont(corrupted_memory)))}")
+
+
+def process_do_dont(text: str) -> str:
+    splitted = text.split("don't()")
+    to_process = splitted[0]
+    for text in splitted[1:]:
+        to_process += text.partition("do()")[2]
+    return to_process
+
 
 if __name__ == "__main__":
     main()
-
-
